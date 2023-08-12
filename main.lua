bf3dab893d57d01c01854ff7c4126840aabe64bf
Artem Dzhemesiuk <programming@zziger.me>
2023-08-12 23:26:39 +0200

    Fixed levelbacks stretching

--- main.lua
+++ main.lua
@@ -308,7 +308,7 @@ local function setLevelBackRenderProjection()
 end
 local setLevelBackRenderProjectionCallback = ffi.cast("void (*)()", setLevelBackRenderProjection)
 
-local function applyCredits()    
+local function applyCredits()  
     creditsDogProjectionAddr:writeFloat(w)
     creditsDogMainProjectionAddr:writeChar(0x68);
     creditsDogMainProjectionAddr:add(1):writeFloat(w * 0.03125);
@@ -317,6 +317,27 @@ local function applyCredits()
     creditsRenderLevelbackAddr:writeNearCall(tonumber(ffi.cast("uint32_t", setLevelBackRenderProjectionCallback)) --[[@as number]])
 end
 --#endregion
+
+--#region Levelbacks
+local widthBuf = ffi.new("float[1]", { w })
+
+local levelbackProj = memory.at("68 ? ? ? ? E8 ? ? ? ? 83 C4 ? 6A ? E8 ? ? ? ? 83 C4 ? 6A ? E8 ? ? ? ? 83 C4 ? 8D 4D"):add(1)
+local levelbacksWidth1 = memory.at("D9 05 ? ? ? ? D8 70"):add(2)
+local levelbacksWidth2 = memory.at("68 ? ? ? ? 8B 4D ? 51 6A ? 8D 8D"):add(1)
+local levelbacksWidth3 = memory.at("D9 05 ? ? ? ? D8 75 ? D8 45 ? 51 D9 1C 24 6A ? 8B 55"):add(2)
+local levelbacksWidth4 = memory.at("D9 05 ? ? ? ? D8 75 ? D8 45 ? 51 D9 1C 24 D9 05"):add(2)
+local levelbacksWidth5 = memory.at("68 ? ? ? ? 8B 45 ? 50 6A ? 8D 8D"):add(1)
+
+local function applyLevelBacks()
+    widthBuf[0] = w
+
+    levelbackProj:writeFloat(w)
+    levelbacksWidth1:writeInt(tonumber(ffi.cast("uintptr_t", widthBuf)))
+    levelbacksWidth2:writeFloat(w)
+    levelbacksWidth3:writeInt(tonumber(ffi.cast("uintptr_t", widthBuf)))
+    levelbacksWidth4:writeInt(tonumber(ffi.cast("uintptr_t", widthBuf)))
+    levelbacksWidth5:writeFloat(w)
+end
 
 --#region Misc
 local resetProjectionAddr = memory.at("68 ? ? ? ? E8 ? ? ? ? 83 C4 ? 5D C3 CC 55"):add(1)
@@ -344,6 +365,7 @@ local function apply()
     applyDialogue()
     applyCredits()
     applyMisc()
+    applyLevelBacks()
     applyZoom()
 end
 
