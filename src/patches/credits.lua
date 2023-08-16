6b0c1c3429783ccb1d3ecefece6d41b14488af23
Artem Dzhemesiuk <programming@zziger.me>
2023-08-16 02:24:03 +0200

    Fixed level end particles

--- src/patches/credits.lua
+++ src/patches/credits.lua
@@ -8,6 +8,7 @@ local creditsRenderLevelbackAddr = memory.at("E8 ? ? ? ? 6A ? E8 ? ? ? ? 83 C4 ?
 local happyEndCreditsBg = memory.at("68 ? ? ? ? 6A ? 6A ? 8D 4D ? E8 ? ? ? ? 8B 50 ? 52 8B 00 50 E8 ? ? ? ? 83 C4 ? D9 05"):add(1)
 local happyEndCredits1Bg = memory.at("68 ? ? ? ? 6A ? 6A ? 8D 4D ? E8 ? ? ? ? 8B 48 ? 51 8B 10 52 E8 ? ? ? ? 83 C4 ? D9 45"):add(1)
 local happyEndWhiteCreditsBg = memory.at("68 ? ? ? ? 6A ? 6A ? 8D 8D ? ? ? ? E8 ? ? ? ? 8B 50"):add(1)
+local levelEndParticleProjection = memory.at("E8 ? ? ? ? 83 C4 ? E8 ? ? ? ? E8 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? E8 ? ? ? ? 83 C4 ? 6A")
 
 local renderLevelbackAddr = memory.at("55 8B EC 81 EC ? ? ? ? D9 05 ? ? ? ? D8 05 ? ? ? ? D9 1D ? ? ? ? 8D 4D ? E8 ? ? ? ? B9")
 local renderLevelback = renderLevelbackAddr:getFunction("void (*)()")
@@ -18,6 +19,12 @@ local function setLevelBackRenderProjection()
     return 0
 end
 local setLevelBackRenderProjectionCallback = ffi.cast("void (*)()", setLevelBackRenderProjection)
+
+local function setLevelEndParticleProjection()
+    setRenderProjection(ratio * 18.75, 18.75, 1, 300)
+    return 0
+end
+local setLevelEndParticleProjectionCallback = ffi.cast("void (*)(float a1, float a2, float a3, float a4)", setLevelEndParticleProjection)
 
 events.on("_unload", function ()
     setLevelBackRenderProjectionCallback:free()
@@ -30,6 +37,7 @@ return function()
     creditsDogMainProjectionAddr:add(5):writeNop(8)
     hiscoreRenderLevelbackAddr:writeNearCall(tonumber(ffi.cast("uint32_t", setLevelBackRenderProjectionCallback)) --[[@as number]])
     creditsRenderLevelbackAddr:writeNearCall(tonumber(ffi.cast("uint32_t", setLevelBackRenderProjectionCallback)) --[[@as number]])
+    levelEndParticleProjection:writeNearCall(tonumber(ffi.cast("uint32_t", setLevelEndParticleProjectionCallback)) --[[@as number]])
     happyEndCreditsBg:writeFloat(w)
     happyEndCredits1Bg:writeFloat(w)
     happyEndWhiteCreditsBg:writeFloat(w)
